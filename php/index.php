68ca5566ca3a26af9f3a0eabd64554aed98cf207
Kesavamoorthi Subramanian <k7moorthi@gmail.com>
2013-11-14 13:12:23 +0530

    intialization

---
+++ php/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Welcome to Hyper Weaver</title>
+</head>
+<body>
+  <h1 style="text-align:center; color:#09F; padding-top:250px;">Website Under Construction!</h1>
+</body>
+</html>
